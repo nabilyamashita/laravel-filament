b76765ba8f79766ba3fd52dacb5defd7f5395d34
nabil <nabilyamashita@gmail.com>
2024-09-24 03:29:29 -1200

    comitan pertama

---
+++ app/Models/Treatment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Patient;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Treatment extends Model
+{
+    use HasFactory;
+
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+}
